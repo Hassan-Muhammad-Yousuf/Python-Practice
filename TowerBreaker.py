aa503a0b0d4ca941ceaec5e230fec7e80d2687a9
Hassan Muhammad Yousuf <alih3766@gmail.com>
2024-07-26 15:41:34 +0200

    11th Problem

---
+++ TowerBreaker.py
@@ -0,0 +1,30 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'towerBreakers' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER m
+#
+
+def towerBreakers(n, m):
+    if n != 1 and n % 2 == 0:
+        return 2
+    else:
+        return 1
+         
+
+if __name__ == '__main__':
+    t = int(input("Enter the number of test cases: "))
+    for _ in range(t):
+        n, m = map(int, input().strip().split())
+        result = towerBreakers(n, m)
+        print(result)
