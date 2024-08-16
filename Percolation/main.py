cf6b0b9b477f4c35a593919c152afa6826a99e88
Hassan Muhammad Yousuf <alih3766@gmail.com>
2024-08-16 16:16:13 +0200

    main

---
+++ Percolation/main.py
@@ -0,0 +1,19 @@
+from Percolation_stats import PercolationStats
+from visual import visual_perc
+
+def main():
+    n = 20 # grid size
+    trials = 100 # number of experiments
+
+    #Run percolation stats
+    stats = PercolationStats(n, trials)
+    print(f"Mean Percolation threshol: {stats.mean():.4f}")
+    print(f"Standard Deviation: {stats.stddev():.4f}")
+    print(f"95% Confidence Intrval: [{stats.confidenceLo():.4f}, {stats.confidenceHi():.4f}]")
+
+    #Visualize percolation process
+    visual_perc(n, pause_time=0.05)
+
+if __name__ == "__main__":
+    main()
+
