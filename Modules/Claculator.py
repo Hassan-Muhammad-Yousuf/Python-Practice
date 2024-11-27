94f155ca5fff1f55de8c96a1aee2d6e81698fc36
Hassan Muhammad Yousuf <alih3766@gmail.com>
2024-11-28 02:21:43 +0500

    practce of Modules

---
+++ Modules/Claculator.py
@@ -0,0 +1,9 @@
+a = 100
+def add(x,y):
+    print("Addition", x + y)
+def sub(x,y):
+    print("Subtraction", x - y)
+def mul(x,y):
+    print("Multipilication", x * y)
+def div(x,y):
+    print("Division", x / y)
---
+++ Modules/test.py
@@ -0,0 +1,7 @@
+import Claculator 
+
+print(Claculator.a)
+Claculator.add(200,300)
+Claculator.sub(200,300)
+Claculator.mul(200,300)
+Claculator.div(200,300)
