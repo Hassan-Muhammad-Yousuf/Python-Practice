e3b3eb5b9533b575e74b0ed37736307f99f7c720
Hassan Muhammad Yousuf <alih3766@gmail.com>
2024-11-28 03:34:46 +0500

    practce of Modules completed

---
+++ Modules/P_Face.py
@@ -0,0 +1,7 @@
+def m1():
+    if __name__ == '__main__':
+        print("Main Program ")
+    else:
+        print("Other Program")
+
+m1()
--- Modules/test.py
+++ Modules/test.py
@@ -1,7 +1,95 @@
-import Claculator 
+# import Claculator as c
 
-print(Claculator.a)
-Claculator.add(200,300)
-Claculator.sub(200,300)
-Claculator.mul(200,300)
-Claculator.div(200,300)
+# print(c.a)
+# c.add(200,300)
+# c.sub(200,300)
+# c.mul(200,300)
+# c.div(200,300)
+
+# from Claculator import a, mul
+
+# print(a)
+# mul(20,20)
+
+# from Claculator import *
+
+# print(a)
+# mul(20,20)
+# div(20,10)
+# add(20,10)
+# sub(20,10)
+
+# from Claculator import a as b, add as sum
+# print(b)
+# sum(100,100)
+
+# import parth
+# import parth
+# import parth
+# import parth
+# import importlib
+# importlib.reload(parth)
+# importlib.reload(parth)
+# print("Hassan", parth.a)
+
+# a = 10
+# b = 20
+
+# def m():
+#     print("Hassan")
+# print(dir())
+
+# import Claculator
+# print(dir(Claculator))
+# print(__builtins__)
+
+# import P_Face
+# P_Face.m1
+
+# from random import *
+# for i in range(6):
+#     print(random())
+
+# from random import *
+# for i in range(6):
+#     print(randint(1,10))
+
+# from random import *
+# for i in range(10):
+#     print(randrange(1,5))
+
+# from random import *
+# l = ["Hassan", "Muhammad", "Yousuf"]
+# for i in range(5):
+#     print(choice(l))
+
+# from datetime import *
+
+# d1 = input("Enter First Date in yyyy-mm-dd format")
+# d2 = input("Enter Second Date in yyyy-mm-dd format")
+
+# d1 = datetime.strptime(d1,"%Y-%m-%d")
+# d2 = datetime.strptime(d2,"%Y-%m-%d")
+
+# diff = abs(d2 - d1)
+
+# print(diff)
+
+# d = datetime.now()
+# a = int(d.timestamp() * 1000)
+# print(a)
+
+# cd = datetime.now()
+# d = timedelta(days=5)
+# nd = cd - d
+# print(nd)
+
+# d = datetime.now().strftime("%H:%M:%S")
+# print(d)
+
+# date_str = datetime.now()
+# date_str = date_str.strftime("%Y-%m-%d %H:%M:%S")
+
+# d = datetime.strptime(date_str, "%Y-%m-%d %H:%M:%S")
+
+# print(d)
