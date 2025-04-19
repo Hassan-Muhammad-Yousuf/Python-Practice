0d0be6a8a7659734281446c8fa6edbf53d5effa9
Hassan Muhammad Yousuf <alih3766@gmail.com>
2025-04-20 01:57:14 +0200

    Speaking Dictionary App

---
+++ Speaking Dict App/main.py
@@ -0,0 +1,32 @@
+import pyttsx3
+from PyDictionary import PyDictionary
+import speech_recognition as sr
+import datetime
+
+class Speaker:
+    def __init__(self, voice = "male"):
+        pass
+    def speak(self, text):
+        pass
+
+class Dict_App:
+    def __init__(self):
+        pass
+    def listen_word(self):
+        pass
+    def get_meaning(self, word):
+        pass
+    def get_synonyms(self, word):
+        pass
+    def get_antonyms(self, word):
+        pass
+    def pronunce(self, word):
+        pass
+    def save_file(self, word, meaning, synonyms, antonyms):
+        pass
+    def run(self):
+        pass
+
+if __name__ = "__main__":
+    app = Dict_App()
+    app.run()
