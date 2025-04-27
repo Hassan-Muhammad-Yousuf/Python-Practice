aa11d4dd9b4436805d2af0f87ff305cd767aeb3c
Hassan Muhammad Yousuf <alih3766@gmail.com>
2025-04-27 03:09:43 +0200

    Exception handling practice

---
+++ Practice/Exception Handling Practise/practise.py
@@ -0,0 +1,22 @@
+# print("Frist-1")
+# print(10/0)
+# print("Second-2")
+
+# print("Frist-1")
+# try:
+#     print(10/0)
+# except ZeroDivisionError as z:
+#     print("Exception:",z)
+
+# print("Second-2")
+
+# try:
+#     a = int(input("Enter Var-1: "))
+#     b = int(input("Enter Var-2: "))
+#     print(a/b)
+# except ZeroDivisionError as z:
+#     print("Exception 1", z)
+# except ArithmeticError as a:
+#     print("Exception 2", a)
+# except ValueError as e:
+#     print("Exception 3",e)
