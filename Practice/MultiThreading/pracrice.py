c330169ba65f9ba64d30d34fbdc220107240b9a0
Hassan Muhammad Yousuf <alih3766@gmail.com>
2025-05-22 16:30:41 +0200

    Multithreading Practice

---
+++ Practice/MultiThreading/pracrice.py
@@ -0,0 +1,13 @@
+# import threading
+# print("Current Exec: Thread ",threading.current_thread().getName())
+
+# import threading
+# def m1():
+#     for i in range(1,5):
+#         print("Child Thread")
+
+# t = threading.Thread(target = m1)
+# t.start()
+# for i in range(1,5):
+#     print("Main Thread")
+
