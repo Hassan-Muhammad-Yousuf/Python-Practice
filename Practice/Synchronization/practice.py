f0d2d475b34a3a4bc911e26c8be302118ece2df4
Hassan Muhammad Yousuf <alih3766@gmail.com>
2025-06-08 03:02:38 +0200

    Sync Practise

--- Practice/Synchronization/practice.py
+++ Practice/Synchronization/practice.py
@@ -77,3 +77,41 @@ from threading import *
 # s.release()
 # s.release()
 # print('end')
+
+
+# from threading import *
+# import time
+# def prod():
+#     time.sleep(3)
+#     print('Producer thread producing items: ')
+#     print('Producer thread notifications: ')
+#     event.set()
+# def cons():
+#     print('Consumer thread waiting :')
+#     event.wait()
+#     print('Consumer thread consuming items :')
+# event = Event()
+# t1 = Thread(target=prod)
+# t2 = Thread(target=cons)
+# t1.start()
+# t2.start()
+
+
+# from threading import *
+# def cons(c):
+#     c.acquire()
+#     print('Consumer thread waiting :')
+#     c.wait()
+#     print('Consumer thread consuming items :')
+#     c.release()
+# def prod(c):
+#     c.acquire()
+#     print('Producer thread producing items: ')
+#     print('Producer thread notifications: ')
+#     c.notify()
+#     c.release()
+# c = Condition()
+# t1 = Thread(target=cons,args=(c,))
+# t2 = Thread(target=prod,args=(c,))
+# t1.start()
+# t2.start()
