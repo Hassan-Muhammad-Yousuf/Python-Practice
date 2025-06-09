7a2c4d14b803ca02e43a5feac195c59306f7036c
Hassan Muhammad Yousuf <alih3766@gmail.com>
2025-06-09 19:35:56 +0200

    practise of RE and WS

---
+++ Practice/Web Scraping and Regular Expressions/practise.py
@@ -0,0 +1,17 @@
+# import re
+# count = 0
+# p = re.compile("pq")
+# m = p.finditer("pqpqpqpq")
+# for m1 in m:
+#     count += 1
+#     print(m1.start(),'-->',m1.end(),'-->',m1.group())
+# print("Number of occurrences: ",count)
+
+# import re
+# count = 0
+
+# m = re.finditer("pq","pqpqpqpq")
+# for m1 in m:
+#     count += 1
+#     print(m1.start(),'-->',m1.end(),'-->',m1.group())
+# print("Number of occurrences: ",count)
